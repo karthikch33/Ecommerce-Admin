9c345fc1f3969c60cabb64cb3f940d5f48367c57
Karthik Ch <saipavan39dh@gmail.com>
2024-05-31 17:06:03 +0530

    url updation corrected

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,3 +1,2 @@
 // const base_url = "http://localhost:1507/api/"
-const base_url = "https://appcbackend.onrender.com/api/"
-export default base_url
+export const base_url = "https://appcbackend.onrender.com/api/"
