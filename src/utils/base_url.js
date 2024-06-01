f7f76f60a3f0f60aff0ea8e46d53b21ba16f00bf
Karthik Ch <saipavan39dh@gmail.com>
2024-06-01 05:32:29 +0530

    package url added api

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,2 +1,2 @@
 // const base_url = "http://localhost:1507/api/"
-export const base_url = "https://appcadminbackend.onrender.com"
+export const base_url = "https://appcadminbackend.onrender.com/api/"
