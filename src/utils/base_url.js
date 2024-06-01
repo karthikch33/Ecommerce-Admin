9e610ef52298e684992b32dc19144f1ae83cd8d8
Karthik Ch <saipavan39dh@gmail.com>
2024-06-01 14:52:48 +0530

    url updated

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,2 +1,2 @@
-export const base_url = "http://localhost:1508/api/"
-// export const base_url = "https://appcadminbackend.onrender.com/api/"
+// export const base_url = "http://localhost:1508/api/"
+export const base_url = "https://appcadminbackend.onrender.com/api/"
